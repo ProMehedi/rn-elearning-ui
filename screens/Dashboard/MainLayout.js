d3935f8b843391d68da7738dbfaf0d875fd109f9
promehedi <admin@promehedi.com>
2022-07-17 12:55:18 +0600

    Create MainLayout

--- screens/Dashboard/MainLayout.js
+++ screens/Dashboard/MainLayout.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import {View, StyleSheet, Animated} from 'react-native';
+import {COLORS, constants, SIZES} from '../../constants';
+import {Home, Search, Profile} from '../../screens';
 
 const MainLayout = () => {
+  const flatListRef = React.useRef();
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  const renderContent = () => {
     return (
-        <View>
-            <Text>MainLayout</Text>
-        </View>
-    )
-}
+      <View style={styles.container}>
+        <Animated.FlatList
+          ref={flatListRef}
+          horizontal
+          pagingEnabled
+          scrollEnabled
+          snapToAlignment="center"
+          snapToInterval={SIZES.width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          data={constants.bottom_tabs}
+          keyExtractor={item => `Main-${item.id}`}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {x: scrollX}}}],
+            {useNativeDriver: false},
+          )}
+          renderItem={({item}) => (
+            <View style={styles.tab}>
+              {item.label === constants.screens.home && <Home />}
+              {item.label === constants.screens.search && <Search />}
+              {item.label === constants.screens.profile && <Profile />}
+            </View>
+          )}
+        />
+      </View>
+    );
+  };
+
+  return <View style={styles.container}>{renderContent()}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  tab: {
+    height: SIZES.height,
+    width: SIZES.width,
+  },
+});
 
 export default MainLayout;
